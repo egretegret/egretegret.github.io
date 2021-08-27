8c873d8b3c462c2add19bc23687438d5e68cba4d
Egret <egret@seas.upenn.edu>
2021-08-27 18:36:17 -0400

    Fix for images using hash router

--- src/Home.js
+++ src/Home.js
@@ -12,21 +12,21 @@ class Home extends React.Component {
                 </h1>
                 <div className="selection-container">
                     <div className="selection-component">
-                        <img className="egg1" src="./egg1.png" alt="animated egg gif"
+                        <img className="egg1" src="./#/egg1.png" alt="animated egg gif"
                             onMouseOver={e => (e.currentTarget.src = "./egg.gif")}
-                            onMouseOut={e => (e.currentTarget.src = "./egg1.png")}></img>
+                            onMouseOut={e => (e.currentTarget.src = "./#/egg1.png")}></img>
                         <h2>programming</h2>
                     </div>
                     <div className="selection-component">
-                        <img className="egg2" src="./egg2.png" alt="animated egg gif"
+                        <img className="egg2" src="./#/egg2.png" alt="animated egg gif"
                             onMouseOver={e => (e.currentTarget.src = "./egg.gif")}
-                            onMouseOut={e => (e.currentTarget.src = "./egg2.png")}></img>
+                            onMouseOut={e => (e.currentTarget.src = "./#/egg2.png")}></img>
                         <h2>about</h2>
                     </div>
                     <div className="selection-component">
-                        <img className="egg3" src="./egg3.png" alt="animated egg gif"
+                        <img className="egg3" src="./#/egg3.png" alt="animated egg gif"
                             onMouseOver={e => (e.currentTarget.src = "./egg.gif")}
-                            onMouseOut={e => (e.currentTarget.src = "./egg3.png")}></img>
+                            onMouseOut={e => (e.currentTarget.src = "./#/egg3.png")}></img>
                         <h2>design</h2>
                     </div>
                 </div>
