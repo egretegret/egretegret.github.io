4ef8c64718e5139e6fb3d0b1d09e96d284b92291
Egret <egret@seas.upenn.edu>
2021-11-07 23:39:55 -0500

    Finish Pagodes page

--- src/dsgn234/projects/fanPage/Pagodes.js
+++ src/dsgn234/projects/fanPage/Pagodes.js
@@ -8,44 +8,78 @@ import Sketch from "react-p5";
 import "./pagodes.css";
 import "p5/lib/addons/p5.sound";
 import pagodes from "./assets/pagodes.mp3";
+import windchimes from "./assets/windchimes.mp3";
+import birds from "./assets/birds.mp3";
+import river from "./assets/river.mp3";
 import { ReactComponent as Settings } from "./assets/settings.svg";
 import { ReactComponent as Home } from "./assets/home.svg";
 
 function Pagodes(props) {
 
-    let sound, amplitude, rotationSlider, sizeSlider, opacitySlider;
+    let music, amplitude, windSound, birdSound, riverSound;
     let duration, current;
-    let progressBar;
+    let rotationSlider, sizeSlider, opacitySlider, progressBar;
 
     const preload = (p5) => {
-        sound = p5.loadSound(pagodes);
+        music = p5.loadSound(pagodes);
+        windSound = p5.loadSound(windchimes);
+        birdSound = p5.loadSound(birds);
+        riverSound = p5.loadSound(river);
     }
 
     const setup = (p5, canvasParentRef) => {
         let cnv = p5.createCanvas(500, 500).parent(canvasParentRef);
-        cnv.mousePressed(mousePressed);
+        cnv.mousePressed(toggleSound);
 
-        duration = sound.duration();
+        duration = music.duration();
+
+        let settingsDiv = document.getElementById("settings");
 
         progressBar = p5.createSlider(0, duration, 1);
         progressBar.addClass("pagodes-progress-bar");
+        progressBar.addClass("content");
 
         rotationSlider = p5.createSlider(0, 360, 108);
-        rotationSlider.position(10, 10);
         rotationSlider.addClass("pagodes-rotation-slider");
-        sizeSlider = p5.createSlider(0, 1000, 500);
-        sizeSlider.position(10, 50);
+        rotationSlider.addClass("pagodes-setting-slider");
+        let rotationLabel = document.createElement("div");
+        rotationLabel.classList.add("label");
+        rotationLabel.innerHTML = "windchimes";
+        settingsDiv.appendChild(rotationLabel);
+        rotationSlider.parent(settingsDiv);
+
+        sizeSlider = p5.createSlider(0, 1500, 750);
         sizeSlider.addClass("pagodes-size-slider");
+        sizeSlider.addClass("pagodes-setting-slider");
+        let sizeLabel = document.createElement("div");
+        sizeLabel.classList.add("label");
+        sizeLabel.innerHTML = "bird chatter";
+        settingsDiv.appendChild(sizeLabel);
+        sizeSlider.parent(settingsDiv);
+
         opacitySlider = p5.createSlider(0, 255, 200);
-        opacitySlider.position(10, 90);
         opacitySlider.addClass("pagodes-opacity-slider");
-        
-        sound.play();
+        opacitySlider.addClass("pagodes-setting-slider");
+        let opacityLabel = document.createElement("div");
+        opacityLabel.classList.add("label");
+        opacityLabel.innerHTML = "river";
+        settingsDiv.appendChild(opacityLabel);
+        opacitySlider.parent(settingsDiv);
+
+        music.play();
+        windSound.loop();
+        birdSound.loop();
+        riverSound.loop();
+
+        windSound.setVolume(.01);
+        birdSound.setVolume(.015);
+        riverSound.setVolume(.02);
+
         amplitude = new p5.constructor.Amplitude();
     }
 
     const draw = p5 => {
-        
+
         let rotation = rotationSlider.value();
         let size = sizeSlider.value();
         let opacity = opacitySlider.value();
@@ -58,20 +92,27 @@ function Pagodes(props) {
 
         p5.push();
         p5.angleMode(p5.DEGREES);
-        p5.translate(p5.width/2, p5.height/2);
+        p5.translate(p5.width / 2, p5.height / 2);
         p5.rotate(rotation);
         p5.stroke(0, 0, 0, 0);
         p5.fill(255, 255, 255, mappedOpacity);
         polygon(p5, 0, 0, shapeWidth, 5);
         p5.pop();
 
-        
-        if (sound.isPlaying()) {
-            current = sound.currentTime();
+        let windVol = p5.map(rotation, 0, 1, 0, .001);
+        let birdVol = p5.map(size, 0, 1, 0, .001);
+        let riverVol = p5.map(opacity, 0, 1, 0, .001);
+
+        windSound.setVolume(windVol);
+        birdSound.setVolume(birdVol);
+        riverSound.setVolume(riverVol);
+
+        if (music.isPlaying()) {
+            current = music.currentTime();
         }
-        p5.text(current+ "/"+ duration, 10, 30);
+
         progressBar.value(current);
-        
+
     }
 
     const polygon = (p5, x, y, radius, npoints) => {
@@ -80,29 +121,66 @@ function Pagodes(props) {
         p5.rotate(p5.PI / 10);
         p5.beginShape();
         for (let a = 0; a < p5.TWO_PI; a += angle) {
-          let sx = x + p5.cos(a) * radius;
-          let sy = y + p5.sin(a) * radius;
-          p5.vertex(sx, sy);
+            let sx = x + p5.cos(a) * radius;
+            let sy = y + p5.sin(a) * radius;
+            p5.vertex(sx, sy);
         }
         p5.endShape(p5.CLOSE);
-      }
+    }
 
-    const mousePressed = () => {
-        if (sound.isPlaying()) {
-            sound.pause();
-          } else {
-            sound.play();
-          }
+    const toggleSound = () => {
+        if (music.isPlaying()) {
+            music.pause();
+            windSound.pause();
+            birdSound.pause();
+            riverSound.pause();
+        } else {
+            music.play();
+            windSound.loop();
+            birdSound.loop();
+            riverSound.loop();
+        }
+    }
+
+    function stopSound() {
+        music.stop();
+        windSound.stop();
+        birdSound.stop();
+        riverSound.stop();
+    }
+
+    function toggleSettings() {
+        let settings = document.getElementById("settings");
+        let content = document.getElementsByClassName("content");
+
+        for (let i = 0; i < content.length; i++) {
+            if (content[i].style.display) {
+                content[i].style.display = content[i].style.display === "block" ? "none" : "block";
+            } else {
+                content[i].style.display = "none";
+            }
+        }
+            if (settings.style.display) {
+                settings.style.display = settings.style.display === "none" ? "flex" : "none";
+            } else {
+                settings.style.display = "flex";
+            }
+
+
     }
 
     return (
         <div class="pagodes">
-            <h1 class="title"> Pagodes </h1>
             <div class="header">
-            <Settings />
-            <Link to={props.path}><Home /></Link>
+                <Link to={props.path} onClick={stopSound}><Home /></Link>
+                <Settings onClick={toggleSettings} />
             </div>
-            <Sketch preload={preload} setup={setup} draw={draw}/>
+            <div class="content">
+                <h1 class="title"> Pagodes </h1>
+                <Sketch preload={preload} setup={setup} draw={draw} />
+            </div>
+            <div id="settings">
+            </div>
         </div>
     );
 
